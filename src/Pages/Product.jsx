9110fba4ba2b6267bf5f8ca31c63a31cc426fa04
saluumaa <salumahassan02@gmail.com>
2023-11-16 10:29:37 +0300

    Create product and shop component

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import { useParams } from 'react-router-dom'
+import { ShopContext } from '../Context/ShopContext'
+import BreadCrums from '../Components/BreadCrums/BreadCrums'
+import ProductDisplay from '../Components/ProductDisplay/ProductDisplay'
 
 const Product = () => {
+  const {all_product} = useContext(ShopContext)
+  const {productId} = useParams()
+  const product = all_product.find(item => item.id === Number(productId))
   return (
-    <div>Product</div>
+    <div>
+      <BreadCrums product={product} />
+      <ProductDisplay product={product} />
+      </div>
   )
 }
 
