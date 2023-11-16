fcff28790f62d5a91485c2136b1ca7ad519cd3cc
saluumaa <salumahassan02@gmail.com>
2023-11-16 10:30:46 +0300

    Create signup section

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
-
+import './CSS/LoginSignup.css'
 const LoginSignup = () => {
   return (
-    <div>LoginSignup</div>
+    <div className='login-signup'>
+      <div className="loginsignup-container">
+        <h1>Signup</h1>
+        <div className="loginsignup-fields">
+          <input type="text" placeholder='Your Name' />
+          <input type="email" placeholder='Email Address' />
+          <input type="password" placeholder='Password' />
+        </div>
+        <button>Continue</button>
+        <p className="loginsignup-login">
+          Already have an account? <span>Login Here</span>
+        </p>
+        <div className="login-signup-agree">
+          <input type="checkbox" name='' id='' />
+          <p>
+            by continuing, i agree to the <span>Terms and Conditions</span> and <span>Privacy Policy</span>
+          </p>
+        </div>
+      </div>
+    </div>
   )
 }
 
